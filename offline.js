1e982df69813ef7418311d95bd0921ae76daa943
omb0722 <102577565+omb0722@users.noreply.github.com>
2022-03-29 15:14:04 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1648537232,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/hammer-sheet0.png",
+		"images/hole-sheet0.png",
+		"images/mouse-sheet0.png",
+		"images/mouse-sheet1.png",
+		"images/pain-sheet0.png",
+		"media/mixkit-metal-hammer-hit-833.m4a",
+		"media/mixkit-metal-hammer-hit-833.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
